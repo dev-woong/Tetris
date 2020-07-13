894d611efe14317c76facbbf3eab3e386ce087b7
dev-woong <dev-woong>
2020-07-13 20:45:20 +0900

    Updates

---
+++ precache-manifest.aae8a56bd8e4f0c249270a5bd5013a07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8aae0382b73008247bfee2042e41497",
+    "url": "/Tetris/index.html"
+  },
+  {
+    "revision": "5993b7b35589d5efa9fb",
+    "url": "/Tetris/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "b531b3e986d0727d56c4",
+    "url": "/Tetris/static/js/2.a30b2a45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tetris/static/js/2.a30b2a45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5993b7b35589d5efa9fb",
+    "url": "/Tetris/static/js/main.3677a1fe.chunk.js"
+  },
+  {
+    "revision": "39392fee0dbc08d25f2b",
+    "url": "/Tetris/static/js/runtime-main.d5578710.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Tetris/static/media/logo.5d5d9eef.svg"
+  }
+]);
